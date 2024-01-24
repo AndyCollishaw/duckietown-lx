308e8fc8ff42ae0c2f14dffbb673c2da0798917c
ac <ac@elec390.myguest.virtualbox.org>
2024-01-24 18:52:40 -0500

    code for 22_55_07

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -7,12 +7,10 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
-    #res[100:300, 0:320] = 0.3
-    #res[301:, 50:400] = 0.8
-    #res[301:, 400:590] = -1
-    res[240:320, 80:320] = 0.3
-    res[320:, :320] = 0.3
-    res[240:480, 200:280] = 1
+
+    res[0:240, 320:] = 0.1
+    res[240:, :320] = 0.5
+    res[320:, 320:] = -0.4
     # ---
     return res
 
@@ -21,12 +19,8 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
 def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
-    # these are random values
-    #res[100:300, 321:] = 0.3
-    #res[301:, 400:590] = 0.8
-    #res[301:, 50:400] = -1
-    res[240:320, 320:560] = 0.3
-    res[320:, 320:] = 0.3
-    res[240:480, 360:440] = 1
+    res[0:240, 0:320] = 0.1
+    res[240:, 320:] = 0.5
+    res[320:, :320] = -0.4
     # ---
     return res
