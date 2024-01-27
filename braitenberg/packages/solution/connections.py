58f78892f75feb50dc25eaba5f9c45c7e3e490e7
ac <ac@elec390.myguest.virtualbox.org>
2024-01-26 21:01:53 -0500

    best d45, d30 still broke, d40 still doesnt work, d50 good, best d60

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -10,12 +10,12 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
 
     
     res[400:, :40] =0.5
-    res[360:, 40:80] = 0.5
+    res[360:, 40:80] = 0.2
     res[320:, 80:120] = 0.2
     res[280:, 120:160] = 0.2
     res[240:, 160:200] = 0.2
     res[160:, 200:240] = 0.5
-    res[160:, 240:280] = 0.5
+    res[160:200, 240:280] = 0.5
     res[160:200, 280:320] = 0.5
 
     
@@ -31,7 +31,7 @@ def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
     res[400:, 600:] = 0.5
-    res[360:, 560:600] = 0.5
+    res[360:, 560:600] = 0.2
     res[320:, 520:560] = 0.2
     res[280:, 480:520] = 0.2
     res[240:, 440:480] = 0.5
@@ -39,7 +39,7 @@ def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     res[160:, 360:400] = 0.5
     res[160:320, 320:360] = 0.5
 
-    res[200:, 280:360] = 0.9
+    res[200:, 240:360] = 0.9
     
    
     
